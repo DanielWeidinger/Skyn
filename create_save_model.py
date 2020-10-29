39e8b190e424887eb1397a0d7a0198dfa5b511c5
Daniel Weidinger <daniel.weidinger@hp-weidinger.at>
2020-10-29 11:50:05 +0100

    path

--- create_save_model.py
+++ create_save_model.py
@@ -8,9 +8,12 @@ from Mask.meta.config.coco_config import CocoConfig
 dir_path = os.path.dirname(os.path.realpath(__file__))
 MODEL_DIR = dir_path + "/models/"
 MODEL_PATH = './models/mask_rcnn_moles_0074.h5'#input("Insert the path of your trained model [ Like models/moles.../mask_rcnn_moles_0030.h5 ]: ")
-SAVE_PATH = './models/saved_model/'
+SAVE_PATH = os.path.normpath(dir_path+'/models/saved_model/1/')
 if not os.path.isfile(MODEL_PATH):
     raise Exception(MODEL_PATH + " Does not exists")
+
+if not os.path.exists(SAVE_PATH):
+    os.makedirs(SAVE_PATH, exist_ok=True)
 
 # create and instance of config
 config = CocoConfig()
